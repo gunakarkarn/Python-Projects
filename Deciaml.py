e9a970a643a7a99a489f2c30774fb41900fed312
Gunakar Karn <89537735+gunakarkarn@users.noreply.github.com>
2022-05-23 20:35:14 +0530

    Delete Deciaml.py

--- Deciaml.py
+++
@@ -1,8 +0,0 @@
-def DecimalToBinary(num):
-    
-    if num >= 1:
-        DecimalToBinary(num // 2)
-    print(num % 2, end = '')
-
-dec_val= int(input("Enter a number in decimal: "))
-DecimalToBinary(dec_val) 
